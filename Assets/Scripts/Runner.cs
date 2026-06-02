f1a888fda9622b3751ee573f4687d051ccf3e0f4
macmastm <macmastm@OA-S141-13>
2026-06-02 17:24:11 -0400

    week 5 in class work

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Runner : MonoBehaviour
 {
     public float speed;
+
+    bool isMoving = false;
+    bool isStopped = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,26 @@ public class Runner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.right * speed * Time.deltaTime;
+        if (isMoving == true)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (isStopped == true)
+        {
+           
+
+        }
+
+    }
+
+    public void OnMoveClick()
+    {
+        isMoving = true;
+    }
+
+    public void OnStopClick()
+    { 
+      
     }
 }
